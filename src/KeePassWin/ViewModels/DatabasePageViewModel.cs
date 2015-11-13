a09ba89ab9bb2da65f7861e2798804fe57865fc0
Taylor Southwick <twsouthwick@outlook.com>
2015-11-13 10:08:36 -0800

    Update DatabasePageViewModel to be auto-set

---
+++ src/KeePassWin/ViewModels/DatabasePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KeePass.Models;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using System.Collections.Generic;
+
+namespace KeePassWin.ViewModels
+{
+    public class DatabasePageViewModel : ViewModelBase
+    {
+        private IKeePassDatabase _database;
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            Database = (IKeePassDatabase)e.Parameter;
+        }
+
+        public IKeePassDatabase Database
+        {
+            get { return _database; }
+            set { SetProperty(ref _database, value); }
+        }
+    }
+}
--- src/KeePassWin/Views/DatabasePage.xaml.cs
+++ src/KeePassWin/Views/DatabasePage.xaml.cs
@@ -1,15 +1,10 @@
-﻿using KeePass.Models;
-using KeePassWin.ViewModels;
+﻿using KeePassWin.ViewModels;
 using Prism.Windows.Mvvm;
-using System.Collections.Generic;
 using System.ComponentModel;
 using Windows.UI.Xaml;
 
 namespace KeePassWin.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DatabasePage : SessionStateAwarePage, INotifyPropertyChanged
     {
         public DatabasePage()
@@ -18,20 +13,13 @@ namespace KeePassWin.Views
             DataContextChanged += SecondPage_DataContextChanged;
         }
 
-        protected override void LoadState(object navigationParameter, Dictionary<string, object> pageState)
-        {
-            base.LoadState(navigationParameter, pageState);
-
-            var database = (IKeePassDatabase)navigationParameter;
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public DatabaseViewModel ViewDataContext => DataContext as DatabaseViewModel;
+        public DatabasePageViewModel Model => DataContext as DatabasePageViewModel;
 
         private void SecondPage_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ViewDataContext)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Model)));
         }
     }
 }
