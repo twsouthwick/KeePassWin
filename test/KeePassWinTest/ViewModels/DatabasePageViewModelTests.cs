37ee801b935995d57e68524b230effd80f62d379
Taylor Southwick <twsouthwick@outlook.com>
2015-11-30 20:47:09 -0800

    Add test project

---
+++ test/KeePassWinTest/ViewModels/DatabasePageViewModelTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace KeePassWin.ViewModels
+{
+    public class DatabasePageViewModelTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
