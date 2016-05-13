255a0efe466880c0a7ed5525eb6f6737df5e76fb
Taylor Southwick <twsouthwick@outlook.com>
2016-05-13 17:30:21 -0600

    Conditionally show website launch button

---
+++ src/KeePassControls/UriVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace KeePass.Controls
+{
+    internal class UriVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var entry = value as IKeePassEntry;
+
+            if (string.IsNullOrWhiteSpace(entry?.Url))
+            {
+                return Visibility.Collapsed;
+            }
+
+            Uri uri;
+            return Uri.TryCreate(entry.Url, UriKind.Absolute, out uri) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
