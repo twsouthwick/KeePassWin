bd54ffd6d2d1a9c1b0708d117b3ac5ca3c1632d6
Taylor Southwick <twsouthwick@outlook.com>
2016-07-20 16:01:50 -0700

    Add null object visibility converter

---
+++ src/KeePassControls/NullObjectVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace KeePass.Controls
+{
+    public class NullObjectVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
