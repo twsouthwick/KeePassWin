46da5049e08b8332c2d0535632640eadb4a80afd
Taylor Southwick <twsouthwick@outlook.com>
2016-08-26 10:46:36 -0700

    Clean up entry view dialog

--- src/KeePassWin/Views/EntryViewDialog.xaml.cs
+++ src/KeePassWin/Views/EntryViewDialog.xaml.cs
@@ -1,22 +1,34 @@
-﻿using System;
+﻿using KeePass;
+using Prism.Commands;
+using System;
 using System.Threading.Tasks;
-using KeePass;
+using System.Windows.Input;
 using Windows.UI.Xaml.Controls;
 
 namespace KeePassWin.Views
 {
     public sealed partial class EntryViewDialog : ContentDialog, IEntryView
     {
-        public EntryViewDialog(IKeePassEntry entry)
+        public EntryViewDialog(IClipboard clipboard, IKeePassEntry entry)
         {
             this.InitializeComponent();
 
             Entry = entry;
+            Copy = new DelegateCommand<string>(clipboard.SetText);
         }
+
+        public ICommand Copy { get; }
 
         public IKeePassEntry Entry { get; }
 
         Task IEntryView.ShowAsync() => ShowAsync().AsTask();
+
+        private void PasswordChecked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+
+            passwordBox.PasswordRevealMode = checkBox.IsChecked == true ? PasswordRevealMode.Visible : PasswordRevealMode.Hidden;
+        }
     }
 }
 
