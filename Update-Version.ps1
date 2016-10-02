76385e1a9f3612b52a7b0d86efebb333d530503e
Taylor Southwick <twsouthwick@outlook.com>
2016-10-01 20:13:57 -0700

    Add "Beta" moniker

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -6,6 +6,12 @@ function UpdateVersion($path)
 	Write-Host "Updating $path to $version"
 	[xml]$manifest= get-content $path
 	$manifest.Package.Identity.Version = $version
+	
+	# Remove any beta monikers
+	$manifest.Package.Identity.Name = $manifest.Package.Identity.Name.Replace("Beta", "")
+	$manifest.Package.Properties.DisplayName = $manifest.Package.Properties.DisplayName.Replace("(Beta)", "").Trim()
+	$manifest.Package.Applications.Application.VisualElements.DisplayName = $manifest.Package.Applications.Application.VisualElements.DisplayName.Replace("(Beta)", "").Trim()
+	
 	$manifest.Save($path)
 }
 
