7b93fddb24c76e4d017e1ad9462b4b05bffbd834
Taylor Southwick <twsouthwick@outlook.com>
2016-11-19 20:13:34 -0800

    Add KeyboardShortcuts class

---
+++ src/KeePassLib/ShortcutName.cs
@@ -0,0 +1,18 @@
+﻿namespace KeePass
+{
+    public enum ShortcutName
+    {
+        None,
+        Search,
+        CopyUserName,
+        CopyPassword,
+        OpenUrl,
+        CopyUrl,
+        InsertItem,
+        ViewItem,
+        DuplicateItem,
+        DeleteItem,
+        Close,
+        ShowMenu
+    }
+}
